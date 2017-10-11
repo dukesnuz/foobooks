64122b26bda857ea2e115e8deb3c1269cfd832a3
David Petringa <david@ajaxtransport.com>
2017-10-11 17:12:14 -0400

    Fixed welcome controller

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,22 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 /*
 class WelcomeController extends Controller
 {
-    public function index()
-	{
-		return view('Welcome');
-	}
+
+public function index()
+{
+return view('Welcome');
+}
+}
 */
 /*
-* If controller has only one job can use __invoke do not have to 
+* If controller has only one job can use __invoke do not have to
 * name action ie index() above
 */
+
 class WelcomeController extends Controller
 {
     public function __invoke()
-	{
-		return view('Welcome');
-	}
+    {
+        return view('Welcome');
+    }
 }
